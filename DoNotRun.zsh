5d6cd51abfbde9a85ed50821097824ffcb726c3d
Piipperi <petri.lakso@icloud.com>
2022-03-05 15:50:53 +0200

    Add files via upload

---
+++ DoNotRun.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+open https://youtu.be/KYxsNJYgrgI
+
+sleep 3
+
+open https://youtu.be/8HvpXWNjr7E
+
+sleep 1
+
+cd ~/Desktop
+mkdir Sussy\ Mogus;cd Sussy\ Mogus
+curl -o When\ The\ Impostor\ is\ SUS.jpg https://i.redd.it/4x5s09qrdin61.jpg
+open When\ The\ Impostor\ is\ SUS.jpg
